69f4f92cc3467259eae6fedc93b4c5e09a98c3e8
Nikita Koliadin <qThegamEp@gmail.com>
2023-08-28 21:58:13 +0300

    Init

---
+++ src/test/java/com/qthegamep/spring/modulith/example/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.qthegamep.spring.modulith.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
