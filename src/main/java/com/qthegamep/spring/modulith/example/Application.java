7a04fb0f0106d08e0cdaa1efa196e967fb18c08a
Nikita Koliadin <qThegamEp@gmail.com>
2023-08-29 21:33:18 +0300

    Add simulation to application class

--- src/main/java/com/qthegamep/spring/modulith/example/Application.java
+++ src/main/java/com/qthegamep/spring/modulith/example/Application.java
@@ -1,12 +1,22 @@
 package com.qthegamep.spring.modulith.example;
 
+import com.qthegamep.spring.modulith.example.order.Order;
+import com.qthegamep.spring.modulith.example.order.OrderManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Application.class, args);
+
+        // Simulation
+        configurableApplicationContext.getBean(OrderManagement.class).complete(new Order());
     }
 }
